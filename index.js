dd9b575e8f2da755ee2dace7ab04f5a826e48e2c
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-16 11:12:29 +0600

    vercel deploy and all setup done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 //!Midelware
@@ -20,13 +21,30 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req, res, next) =>{
+  // console.log('verifyJwt has hitting');
+  // console.log(req.headers.authorization);
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.stetus(401).send({error: true, message: 'unauthorized accsess'})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET ,(error, decoded)=>{
+    if(error){
+      return res.stetus(403).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const serviseCallection = client.db("carDoctor").collection("services");
-    const bookingCallection = client.db('carDoctor').collection('booking')
+    const bookingCallection = client.db("carDoctor").collection("booking");
 
     app.get("/services", async (req, res) => {
       const cursor = serviseCallection.find();
@@ -34,53 +52,67 @@ async function run() {
       res.send(rusult);
     });
 
-    app.get("/services/:id", async(req,res)=>{
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const options = {
         // Include only the `title` and `imdb` fields in each returned document
         projection: { service_id: 1, price: 1, title: 1, _id: 1, img: 1 },
       };
-      const result = await serviseCallection.findOne(query, options)
-      res.send(result)
-    })
+      const result = await serviseCallection.findOne(query, options);
+      res.send(result);
+    });
 
-    app.post('/booking', async(req, res)=>{
+    app.post("/booking", async (req, res) => {
       const booking = req.body;
-      const result = await bookingCallection.insertOne(booking)
-      res.send(result)
-    })
+      const result = await bookingCallection.insertOne(booking);
+      res.send(result); 
+    });
 
-    app.get('/booking', async(req,res)=>{
-      console.log(req.query.email);
-      let query = {}
-      if(req.query?.email){
-        query = {email: req.query.email}
+    //! jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '24h',
+      });
+      res.send({token})
+    });
+
+    app.get("/booking", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if(decoded.email !== req.query?.email){
+        return res.status(401).send({error: 1, message: 'unauthorized access'})
+      }
+      // console.log(decoded);
+      // console.log(req.headers.authorization, 'heello');
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
       const result = await bookingCallection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.delete('/booking/:id', async(req,res)=>{
+    app.delete("/booking/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await  bookingCallection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCallection.deleteOne(query);
+      res.send(result);
+    });
 
-    app.patch('/booking/:id', async(req,res)=>{
+    app.patch("/booking/:id", async (req, res) => {
       const id = req.params.id;
       const bookingUpdate = req.body;
       console.log(bookingUpdate);
       const updateDoc = {
         $set: {
-          stetus: bookingUpdate.stetus
-        }
-      }
-      const filter = {_id: new ObjectId(id)}
-      const result = await bookingCallection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
+          stetus: bookingUpdate.stetus,
+        },
+      };
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookingCallection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -99,5 +131,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Car doctor server is running on${port}`);
+  console.log(`Car doctor server is running on ${port}`);
 });
